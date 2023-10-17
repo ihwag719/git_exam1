9e3c147d7f542767b655ea6a07094963d308cb0e
kimihwa <ihwag719@gmail.com>
2023-10-18 03:16:04 +0900

    202112116-2

--- src/git_exam1/Hello.java
+++ src/git_exam1/Hello.java
@@ -3,6 +3,7 @@ package git_exam1;
 public class Hello {
 
 	public static void main(String[] args) {
+		System.out.println("IT융합자율학부");
 		System.out.println("202112116");
 		System.out.println("김이화");
 	}
