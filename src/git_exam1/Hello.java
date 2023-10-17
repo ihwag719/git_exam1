912a8e9b394f1f93135beee3f8be48423a89dbab
kimihwa <ihwag719@gmail.com>
2023-10-18 03:21:18 +0900

    202112116-4

--- src/git_exam1/Hello.java
+++ src/git_exam1/Hello.java
@@ -3,8 +3,8 @@ package git_exam1;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("IT융합자율학부");
-		System.out.println("202112116");
-		System.out.println("김이화");
+		System.out.println("소속: IT융합자율학부");
+		System.out.println("학번: 201014099");
+		System.out.println("이름: 홍길동");
 	}
 }
